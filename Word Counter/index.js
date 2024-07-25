6a1b112a4be1f07c54644b8b90bbed6a6118beb4
Istiak Rahman <85867047+legendfahim@users.noreply.github.com>
2024-07-25 20:39:26 +0600

    Separated all projects

--- Word Counter/index.js
+++
@@ -1,5 +0,0 @@
-function count() {
-  let text = document.getElementById("textarea");
-  document.getElementById("number").textContent = wordCount(text.value);
-}
-document.getElementById("b").addEventListener("click", count);
